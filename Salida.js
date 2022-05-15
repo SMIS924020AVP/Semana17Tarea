044a9f6c2c9dc2000c4f4e88dd8fd6cbeae6f78d
Angel <villatoroperez2000@gmail.com>
2022-05-15 11:13:23 -0600

    Correccion de errores.

--- Salida.js
+++ Salida.js
@@ -1,7 +1,6 @@
 var getAge = require('./index.js');
-	
+console.log("Has vivido:")	
 console.log(getAge.fullAge("12/16/2001"))
-console.log(getAge.inMonths("12/16/2001"))
 console.log(getAge.inHours("12/16/2001"))
 console.log(getAge.inMinutes("12/16/2001"))
 console.log(getAge.inSeconds("12/16/2001"))
